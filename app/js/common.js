e05b1b21b616ee75990597d669e11b9f7bf953f1
Your Name <you@example.com>
2021-11-10 17:17:48 +0200

    message commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,16 +1,38 @@
-const btn = document.querySelector('#join')
-const btnTarget = document.querySelector('.reg')
-const btnExit = document.querySelector('.reg__exit')
+// Register window (Volunteer)
+if (document.querySelector('.reg')) {
+	const btnJoin = document.querySelector('#join')
+	const btnTarget = document.querySelector('.reg')
+	const btnExit = document.querySelector('.reg__exit')
 
-btn.addEventListener('click', function (item) {
-	btnTarget.classList.add('active')
+	btnJoin.addEventListener('click', function (item) {
+		btnTarget.classList.add('active')
+	})
 
-	console.log()
+	btnExit.addEventListener('click', function (item) {
+		btnTarget.classList.remove('active')
+	})
 
-})
+}
 
-btnExit.addEventListener('click', function (item) {
-	btnTarget.classList.remove('active')
+
+// Tabs (Donation)
+
+const tabsBtn = document.querySelectorAll('.protect__tabs-btn')
+const tabsContent = document.querySelectorAll('.protect__tabs-content')
+
+tabsBtn.forEach(function (tab, index) {
+	tab.addEventListener('click', function (item) {
+		tabsBtn.forEach(function (item) {
+			item.classList.remove('active')
+		})
+
+		tabsContent.forEach(function (item) {
+			item.classList.remove('active')
+		})
+
+		tabsContent[index].classList.add('active')
+		tab.classList.add('active')
+	})
 })
 
 
